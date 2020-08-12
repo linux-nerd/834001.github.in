0feed6c9afab7b16fa64674678452395eb473c63
Aishwarya <aishwaryasinha0104@gmail.com>
2020-08-12 18:09:44 +0530

    myFirstWebpage

---
+++ Index.js
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible"content="ie=edge">
+    <title>Best Online Food Service in India | My Onlinemeal.com</title>
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet"  media="screen and(max-width: 984px)" href="css/phone.css">
+<link href="https://fonts.googleapis.com/css2?family=Baloo+Bhai+2:wght@700&family=Baloo+Bhaina+2:wght@700&family=Bree+Serif&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+    <nav id="navbar">
+        <div id="logo">
+            <img src="logo.jpg" alt="My Onlinemeal.com" >
+        </div>
+        <ul>
+            <li class="item"><a href="#">Home</a></li>
+            <li class="item"><a href="#">Services</a></li>
+            <li class="item"><a href="#">About Us</a></li>
+            <li class="item"><a href="#">Contact Us</a></li>
+        </ul>
+     </nav>
+
+     <section id="home">
+       <h1 class="h-primary">Welcome to MyOnlineMeal</h1>
+       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius ipsam tenetur reprehenderit?</p>
+       <p>Lorem ipsum dolor sit amet, consectetur . Doloremque iusto mollitia, distinctio soluta </p>
+       <button class="btn">Order Now</button>
+     </section>
+
+     <section class="Services-container">
+         <h1 class="h-primary center">Our Services</h1>
+         <div id="services">
+             <div class="box">
+               <img src="img/../pic1.jpg" alt="">
+               <h2 class="h-secondary center">Food Catering</h2>
+               <p class="center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic aut recusandae similique quae ea, neque modi molestias laudantium reiciendis, quo harum pariatur porro magni inventore iure quidem libero! Soluta molestiae saepe suscipit cumque tenetur!</p>
+             </div>  
+             <div class="box">
+                <img src="https://cdn.pixabay.com/photo/2018/03/28/20/32/food-3270461_960_720.jpg" alt="">
+                <h2 class="h-secondary center">Bulk Ordering</h2>
+                <p class="center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic aut recusandae similique quae ea, neque modi molestias laudantium reiciendis, quo harum pariatur porro magni inventore iure quidem libero! Soluta molestiae saepe suscipit cumque tenetur!</p>
+              </div>
+              <div class="box">
+                <img src="https://img.favpng.com/11/24/1/online-food-ordering-food-delivery-restaurant-grubhub-png-favpng-UWiXQaLWWayatvHynZf31yPuN.jpg" alt="">
+                <h2 class="h-secondary center">Food Ordering</h2>
+                <p class="center" >Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hic aut recusandae similique quae ea, neque modi molestias laudantium reiciendis, quo harum pariatur porro magni inventore iure quidem libero! Soluta molestiae saepe suscipit cumque tenetur!</p>
+              </div>
+             
+         </div>
+     </section>
+     <section id="client-section">
+         <h1 class="h-primary center">Our Clients</h1>
+         <div id="clients">
+             <div class="client-item">
+                 <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8TEuGMdNjDr9EH9rw3hU4FeAJo0JONnVQLg&usqp=CAU" alt="" srcset="">
+             </div>
+             <div class="client-item">
+                <img src="https://www.nowmd.com/wp-content/uploads/Microsoft-Logo.png "alt="" srcset="">
+            </div>
+            <div class="client-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcS8TEuGMdNjDr9EH9rw3hU4FeAJo0JONnVQLg&usqp=CAU" alt="" srcset="">
+            </div>
+            <div class="client-item">
+                <img src="https://www.computing.co.uk/api/v1/wps/710d206/5fb4089a-e9ec-4795-81c5-c92cd4c53527/2/skype-logo-580x358.jpg" alt="" srcset="">
+            </div>
+            <div class="client-item">
+                <img src="https://i0.wp.com/www.newsgram.com/wp-content/uploads/2018/06/HP.jpg" alt="" srcset="">
+            </div>
+         </div>
+     </section>
+     <section id="contact">
+         <h1 class="h-primary center">Contact Us</h1>
+         <div id="contact-box">
+             <form action="">
+                 <div class="form-group">
+                     <label for="name">Name:</label>
+                     <input type="text" name="name" id="name" placeholder="Enter your name">
+                 </div>
+                 <div class="form-group">
+                    <label for="email">Email:</label>
+                    <input type="email" name="name" id="email" placeholder="Enter your email">
+                </div>
+                <div class="form-group">
+                    <label for="phone">Phone Number:</label>
+                    <input type="phone" name="name" id="phone" placeholder="Enter your phone">
+                </div>
+                <div class="form-group">
+                    <label for="message">Message:</label>
+                    <textarea name="message" id="message" cols="30" rows="10"></textarea>
+                </div>
+             </form>
+         </div>
+     </section>
+     <footer>
+         <div class="center">
+             copyright &copy; www.MyOnlineMeal.com. All rights reserved!
+         </div>
+     </footer>
+</body>
+</html>
